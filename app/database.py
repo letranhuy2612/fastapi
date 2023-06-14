fa05a7b0dd0e11d6839c231d0a2510d147098dc6
letranhuy2612 <85574548+letranhuy2612@users.noreply.github.com>
2023-06-14 10:22:59 +0700

    Update database.py

--- app/database.py
+++ app/database.py
@@ -3,6 +3,20 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .config import settings
 
+def check_name(name):
+    """
+        Check database name.
+    """
+    if not name:
+        raise Exception("database name cannot be the empty string")
+
+    invalid_chars = [" ", ".", "$", "/",'@']
+    for invalid_char in invalid_chars:
+        if invalid_char in name:
+            raise Exception(f"database names cannot contain the character {invalid_char}")
+
+check_name(settings.POSTGRES_DB)
+
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOSTNAME}:{settings.DATABASE_PORT}/{settings.POSTGRES_DB}"
 
 engine = create_engine(
